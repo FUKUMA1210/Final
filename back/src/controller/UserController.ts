1c53ea8f2e2fffdff84835d6bd7084a270d6c6de
yuchi822 <411630303@o365.tku.edu.tw>
2025-01-11 01:30:46 +0800

    Update

--- back/src/controller/UserController.ts
+++ back/src/controller/UserController.ts
@@ -1,44 +1,49 @@
 import { Controller } from "../abstract/Controller";
-import { Request, response, Response } from "express";
+import { Request, Response } from "express";
 import { UserService } from "../Service/UserService";
 import { resp } from "../utils/resp";
 import { DBResp } from "../interfaces/DBResp";
 import { Student } from "../interfaces/comments";
-require('dotenv').config()
+
+require("dotenv").config();
 
 export class UserController extends Controller {
-    protected service: UserService;
+  protected service: UserService;
 
-    constructor() {
-        super();
-        this.service = new UserService();
+  constructor() {
+    super();
+    this.service = new UserService();
+  }
+
+  public async findAll(req: Request, res: Response) {
+    const response: resp<Array<DBResp<Student>> | undefined> = {
+      code: 200,
+      message: "",
+      body: undefined,
+    };
+
+    try {
+      const dbResp = await this.service.getAllStudents();
+      if (dbResp) {
+        response.body = dbResp;
+        response.message = "Find success";
+        res.send(response);
+      } else {
+        throw new Error("Server error");
+      }
+    } catch (error) {
+      response.code = 500;
+      response.message = "Server error";
+      res.status(500).send(response);
     }
+  }
 
-    public async findAll(Request: Request, Response: Response) {
-
-        const res: resp<Array<DBResp<Student>> | undefined> = {
-            code: 200,
-            message: "",
-            body: undefined
-        }
-
-        const dbResp = await this.service.getAllStudents();
-        if (dbResp) {
-            res.body = dbResp;
-            res.message = "find sucess";
-            Response.send(res);
-        } else {
-            res.code = 500;
-            res.message = "server error";
-            Response.status(500).send(res);
-        }
-
+  public async insertOne(req: Request, res: Response) {
+    try {
+      const resp = await this.service.insertOne(req.body);
+      res.status(resp.code).send(resp);
+    } catch (error) {
+      res.status(500).send({ code: 500, message: "Server error" });
     }
-
-    public async insertOne(Request: Request, Response: Response) {
-        const resp = await this.service.insertOne(Request.body)
-        Response.status(resp.code).send(resp)
-    }
-
-
+  }
 }
