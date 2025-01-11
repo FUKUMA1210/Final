a95101177d7d0984e170ada09e78cbf151c8f498
shii128037 <shii128037@gmail.com>
2025-01-11 22:24:26 +0800

    Add files via upload

---
+++ front/src/component/Footer.tsx
@@ -0,0 +1,12 @@
+import '../style/Footer.css';
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <footer>
+                <hr className="footer-divider" />
+                <p><a href="https://github.com/FUKUMA1210/Final.git">Powered by Group 6 - 2025</a></p>
+            </footer>
+        </div>
+    );
+}
